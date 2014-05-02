376bd52a614a6c43b9af3c59c3d13d01b1c75df7
Julio Cesar de Antolin González-Cueva <deantolinglez@gmail.com>
2014-05-02 19:18:52 +0200

    Fusion BD con frontend y backend

---
+++ app/business/GeneralAdminService.java
@@ -0,0 +1,19 @@
+package business;
+
+import models.AbstractOrganization;
+import models.UploadDocs;
+import models.User;
+
+import models.exception.BusinessException;
+
+public interface GeneralAdminService {
+	
+	public void updateONG(AbstractOrganization organization) throws BusinessException;
+	
+	public void updateDocs(UploadDocs doc) throws BusinessException;
+	
+	public void addExternalSource(String link, User user) throws BusinessException;
+	
+	public void updateExternalSource(UploadDocs doc) throws BusinessException;
+
+}
