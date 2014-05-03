59dd50b67665a760fe19570235f5b912649be16d
Miriam Abalo Fernández <mirylynx@gmail.com>
2014-05-03 18:45:18 +0200

    UpdateDocs

--- app/controllers/generalAdmin/UpdateDocs.java
+++ app/controllers/generalAdmin/UpdateDocs.java
@@ -1,5 +1,41 @@
 package controllers.generalAdmin;
 
-public class UpdateDocs {
+import static play.data.Form.form;
+import model.UploadDocs;
+import model.exception.BusinessException;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import conf.ServicesFactory;
+import controllers.routes;
 
+public class UpdateDocs extends Controller {
+
+	private UploadDocs doc;
+
+	public String get() {
+		try {
+			ServicesFactory.getGeneralAdminService().updateDocs(doc);
+			return null;
+		} catch (BusinessException e) {
+			return e.getMessage();
+		}
+	}
+
+	public static Result updateONG() {
+		return ok();
+	}
+
+	/**
+	 * Handle login form submission.
+	 */
+	public static Result post() {
+		Form<UpdateExternalSource> form = form(UpdateExternalSource.class)
+				.bindFromRequest();
+		if (form.hasErrors()) {
+			return badRequest();
+		} else {
+			return redirect(routes.Application.index());
+		}
+	}
 }
