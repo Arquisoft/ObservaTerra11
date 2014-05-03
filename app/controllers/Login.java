22b947dfa18f44d6401224d1fba339080ce05991
Marta Jiménez Franco <tetaliuss@gmail.com>
2014-05-03 12:15:09 +0200

    Finish removing compile errors

--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -1,11 +1,14 @@
 package controllers;
 
+import static play.data.Form.form;
 import models.UserLabra;
+import play.data.Form;
 import play.mvc.Controller;
-
+import play.mvc.Result;
+import views.html.*;
 
 public class Login extends Controller {
-
+	
 	public String email;
 	public String password;
 
@@ -15,22 +18,23 @@ public class Login extends Controller {
 		}
 		return null;
 	}
-
-	// public static Result login() {
-	// return ok(login.render(form(Login.class)));
-	// }
+	
+	public static Result login() {
+		return ok(login.render(form(Login.class)));
+	}
 
 	/**
 	 * Handle login form submission.
 	 */
-	// public static Result authenticate() {
-	// Form<Login> loginForm = form(Login.class).bindFromRequest();
-	// if (loginForm.hasErrors()) {
-	// return badRequest(login.render(loginForm));
-	// } else {
-	// session("email", loginForm.get().email);
-	// return redirect(routes.Application.index());
-	// }
-	// }
+	public static Result authenticate() {
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest(login.render(loginForm));
+		} else {
+			//TODO
+			session("email", loginForm.get().email);
+			return redirect(routes.Application.index());
+		}
+	}
 
 }
