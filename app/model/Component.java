17353926ac27bb019186cec40aa6a27f6049bacd
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2014-05-04 18:15:40 +0200

    DB commit

--- app/model/Component.java
+++ app/model/Component.java
@@ -3,6 +3,7 @@ package model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -13,7 +14,9 @@ public class Component implements Serializable {
 
 	private static final long serialVersionUID = 733926216731815257L;
 
-	//Se ha quitado el @id porque no funcionaba
+	@Id
+	private Long id;
+	 // se quito por esa mierda porque no funcionaba
 	@ManyToOne
 	private AbstractOrganization orgHave;
 	
