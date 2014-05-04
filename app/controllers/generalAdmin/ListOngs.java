8c942bd045d38a13559d0e4db72801d66bf5dfac
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2014-05-04 11:33:21 +0200

    Comment to compile project

--- app/controllers/generalAdmin/ListOngs.java
+++ app/controllers/generalAdmin/ListOngs.java
@@ -8,11 +8,12 @@ import conf.ServicesFactory;
 public class ListOngs extends Controller {
 	
 	 public static Result showOngs() {
-		 try{
-	        return ok(ongs.render(ServicesFactory.getGeneralAdminService().listOngs()));
-		 } catch(BusinessException e){
-			 return ok(fail.render(e.getMessage()));
-		 }
+		// try{
+	     //   return ok(ongs.render(ServicesFactory.getGeneralAdminService().listOngs()));
+		 //} catch(BusinessException e){
+		//	 return ok(fail.render(e.getMessage()));
+		 //}
+		 return null;
 	    }
 
 }
