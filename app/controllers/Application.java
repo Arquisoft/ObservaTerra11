7b307fdfc9f170864c4809707b65d30f685d9f50
Luis Alberto Rojo Roldan <luiso54@gmail.com>
2014-05-05 10:10:00 +0200

    añadiendo mas vistas con controlador y routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,19 @@
 package controllers;
 
+import static play.data.Form.form;
 import model.Country;
 import model.Indicator;
 import model.Observation;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import controllers.general.Register;
+import views.html.*;
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok("index");
+		return ok(index.render());
 	}
 
 	public static Result showCountries() {
@@ -27,6 +30,14 @@ public class Application extends Controller {
 
 	public static Result bars(String indicator) {
 		return ok("bars");
+	}
+	
+	public static Result administrador(){
+		return ok(adminPanel.render());
+	}
+	
+	public static Result adminAnadir(){
+		return ok(adminAnadirUsuario.render(form(Register.class)));
 	}
 
 	static Form<Country> countryForm = Form.form(Country.class);
