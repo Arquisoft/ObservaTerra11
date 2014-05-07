56cb80545f73630a03bba9f8c0e1d4a0d74d615a
Luis Alberto Rojo Roldan <luiso54@gmail.com>
2014-05-07 17:37:07 +0200

    desarrollando lista de usuarios

---
+++ app/business/impl/generalAdmin/ListUsers.java
@@ -0,0 +1,19 @@
+package business.impl.generalAdmin;
+
+import java.util.List;
+
+import persistence.AdminFinder;
+import model.AbstractOrganization;
+import model.exception.BusinessException;
+import business.impl.Command;
+
+public class ListOngs implements Command{
+
+	public Object execute() throws BusinessException {
+
+		List<AbstractOrganization> orgs = AdminFinder.listUsers();
+
+		return orgs;
+	}
+
+}
