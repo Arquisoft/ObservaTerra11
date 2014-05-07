e68bbfd23d1cc4ed18b8f0ab5fff3acec10f3398
Luis Alberto Rojo Roldan <luiso54@gmail.com>
2014-05-07 20:03:58 +0200

    reparado

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,11 +6,14 @@ import model.Observation;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.*;
+import controllers.general.*;
+
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok("index");
+		return ok(index.render());
 	}
 
 	public static Result showCountries() {
@@ -28,9 +31,20 @@ public class Application extends Controller {
 	public static Result bars(String indicator) {
 		return ok("bars");
 	}
-
-	static Form<Country> countryForm = Form.form(Country.class);
-	static Form<Indicator> indicatorForm = Form.form(Indicator.class);
-	static Form<Observation> observationForm = Form.form(Observation.class);
-
+	
+	public static Result administrador(){
+		return ok(adminPanel.render());
+	}
+	
+//	public static Result adminAnadir(){
+//		return ok(adminAnadirUsuario.render(form(Register.class)));
+//	}
+	
+//	public static Result adminBorrarUsuario(){
+//		return DeleteUser.deleteUser();
+//	}
+	
+	public static Result adminListarUsuarios(){
+		return ok();//adminListarUsuarios.render(ServicesFactory.);
+	}
 }
