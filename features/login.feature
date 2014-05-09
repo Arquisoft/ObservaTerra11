a55d35f8459bf92b6ffe43322b26f7dbd3534625
Miriam Abalo Fernández <mirylynx@gmail.com>
2014-05-09 13:17:56 +0200

    yeah babe

---
+++ features/login.feature
@@ -0,0 +1,12 @@
+# language: es
+Característica: Página principal
+
+  Como un usuario comun
+  Quiero poder loguearme
+  Para poder acceder a las opciones de la aplicacion
+
+Escenario: 
+    
+	Dado que voy a la pagina de logueo
+	Cuando introduzco labra y asd
+	Entonces me logueo con exito
--- test/web/WebSteps.java
+++ test/web/WebSteps.java
@@ -19,16 +19,16 @@ public class WebSteps {
 	@Named("PORT")
 	private Integer port;
 
-	@Dado("^que tengo arrancada la aplicación$")
+	@Dado("^que tengo arrancada la aplicacion$")
 	public void que_tengo_arrancada_la_aplicación() throws Throwable {
 	}
 
-	@Cuando("^voy a la página principal$")
+	@Cuando("^voy a la pagina principal$")
 	public void voy_a_la_página_principal() throws Throwable {
 		testBrowser.goTo("http://localhost:" + port);
 	}
 
-	@Entonces("^el título es (.+)$")
+	@Entonces("^el titulo es (.+)$")
 	public void el_título_es(String title) throws Throwable {
 		assertThat(testBrowser.title()).isEqualTo(title);
 	}
